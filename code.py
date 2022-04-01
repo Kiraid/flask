57ae0160a76c0450bafb5fa23e94dcf924e1aed1
Kiraid <90769364+Kiraid@users.noreply.github.com>
2022-04-01 21:43:03 +0500

    Update code.py

--- code.py
+++ code.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
     
+
+   
